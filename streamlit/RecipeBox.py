f26c70ddbec50568befde752dbb94434b61db870
ipekgamzeucal <ipekgamzeucal@gmail.com>
2023-05-24 22:36:16 +0200

    Update RecipeBox.py

--- streamlit/RecipeBox.py
+++ streamlit/RecipeBox.py
@@ -21,7 +21,7 @@ from PIL import Image
 
 def main():
     #image = Image.open('https://raw.githubusercontent.com/ipekgamzeucal/RecipeBox/main/streamlit/assets/simgemiz.png')
-    #image = Image.open('../assets/simgemiz.png')
+    image = Image.open('../assets/simgemiz.png')
     st.image(image, caption='', use_column_width=False)
     st.markdown(
         """
